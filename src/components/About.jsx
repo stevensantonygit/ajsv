b569b186812414c334d19418542d2cf74f6817e1
Steven Antony <stevensantony27@gmail.com>
2025-07-01 21:47:29 +0800

    AJSV

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+import AnimatedTitle from "./AnimatedTitle";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+    useGSAP(() => {
+        const clipAnimation = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#clip",
+                start: "center center",
+                end: "+=800 center",
+                scrub: 0.5,
+                pin: true,
+                pinSpacing: true,
+            },
+        });
+
+        clipAnimation.to(".mask-clip-path", {
+            width: "100vw",
+            height: "100vh",
+            borderRadius: 0,
+        });
+    });
+
+    return (
+        <div id="about" className="min-h-screen w-screen">
+            <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+                <p className="font-general text-sm uppercase md:text-[10px]">
+                    Welcome to AJSV
+                </p>
+
+                <AnimatedTitle
+                    title="Where curious minds expl<b>o</b>re, build, and <br /> hack t<b>o</b>gether"
+                    containerClass="mt-5 !text-black text-center"
+                />
+
+                <div className="about-subtext">
+                    <p>Fueling innovation, one project at a time</p>
+                    <p className="text-gray-500">
+                        AJSV is part of Hack Club, a global network where students learn by making. We code, create, and collaborate in the spirit of fun and learning.
+                    </p>
+                </div>
+            </div>
+
+            <div className="h-dvh w-screen" id="clip">
+                <div className="mask-clip-path about-image">
+                    <img
+                        src="public/img/about.webp"
+                        alt="Background"
+                        className="absolute left-0 top-0 size-full object-cover"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
